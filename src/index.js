3995b5cbf0f5e7065f8383b38198dfeeaa5b0d7d
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-14 11:16:31 -0800

    add branch, html layout

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import *** from './js/*** */.js';
+
+$(document).ready(function() {
+  console.log("hey");
+});
