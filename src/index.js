3fbcf1135d87fb36375c2ec666896bcc56840d73
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-02-15 13:28:04 -0800

    Add scroll functionality in side bar
    Co-Authored-By: Hayley M <hayleymcvay8@gmail.com>

--- src/index.js
+++ src/index.js
@@ -139,7 +139,7 @@ $("ul.category").on("click", "li", function () {
 });
 
 $("#top").click(function () {
-  $("html, body").animate({ scrollTop: 0 }, "slow");
+  $("html, body").animate({ scrollTop: 0 }, "fast");
 });
 
 $("form#ingredientsInput").submit(function (event) {
